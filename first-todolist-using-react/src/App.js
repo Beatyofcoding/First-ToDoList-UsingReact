d9ef2cb6ebf1bc9ea2ac0a10ad3120f81af174b8
Beatyofcoding <60256215+Beatyofcoding@users.noreply.github.com>
2020-08-01 16:44:25 +0200

    DeletingCode&RestartingAgain

--- first-todolist-using-react/src/App.js
+++ first-todolist-using-react/src/App.js
@@ -2,16 +2,6 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <header>
-      <form id="to-do-form">
-        
-      </form>
-    </header>
-    <h1>First ToDo List Uusing React</h1>
-    
-  );
-}
+
 
 export default App;
