813bfa3bcc86fcc120d88e283f01e9e95e04e041
Beatyofcoding <60256215+Beatyofcoding@users.noreply.github.com>
2020-08-01 16:40:59 +0200

    StartingWithFrontEndPart

--- first-todolist-using-react/src/App.js
+++ first-todolist-using-react/src/App.js
@@ -4,6 +4,11 @@ import './App.css';
 
 function App() {
   return (
+    <header>
+      <form id="to-do-form">
+        
+      </form>
+    </header>
     <h1>First ToDo List Uusing React</h1>
     
   );
