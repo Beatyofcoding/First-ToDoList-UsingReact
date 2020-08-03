4fb4b0bfa1ad5bcbdab79138b195f160c0cc8bdf
Beatyofcoding <60256215+Beatyofcoding@users.noreply.github.com>
2020-08-03 13:41:54 +0200

    EncloseTheEntireFormUnderDivClassName"App"

--- first-todolist-using-react/src/App.js
+++ first-todolist-using-react/src/App.js
@@ -5,13 +5,16 @@ import './App.css';
 class App extends React.Component{
   render(){
     return(
-          <header>
+      <div className="App">
+                  <header>
             <form id="to-do-form">
               <input type="text" placeholder="Enter Text Here" />
               <button type="submit">Add</button>
 
             </form>
           </header>   
+      </div>
+
            );
   }
 }
