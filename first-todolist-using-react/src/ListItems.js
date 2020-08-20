644791f2365c47f1378648e2460b10e8e42295a5
Beatyofcoding <60256215+Beatyofcoding@users.noreply.github.com>
2020-08-20 17:14:13 +0200

    SettinKeyAs"item.keyForTheCreatdDivClasForUniqueID

--- first-todolist-using-react/src/ListItems.js
+++ first-todolist-using-react/src/ListItems.js
@@ -5,7 +5,9 @@ function ListItems(props){
     const items = props.items;
     const listItems = items.map(item =>
         {
-            return <div className="list"></div>
+            return <div className="list" key="item.key">
+                
+            </div>
         })
     return(
 
