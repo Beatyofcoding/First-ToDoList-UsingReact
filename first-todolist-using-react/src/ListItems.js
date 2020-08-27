bd0d2000f202744fd39cfbf175525f0a58ce3763
Beatyofcoding <60256215+Beatyofcoding@users.noreply.github.com>
2020-08-27 04:33:38 +0200

    TryingToSetTrashIconAFterParagraph&Testing

--- first-todolist-using-react/src/ListItems.js
+++ first-todolist-using-react/src/ListItems.js
@@ -8,10 +8,12 @@ function ListItems(props){
     const listItems = items.map(item =>
         {
             return <div className="list" key="item.key">
+                <p>{item.text}
                 <span>
                     <FontAwesomeIcon className="faicons" icon="trash" />
                 </span>
-                <p>{item.text}</p>
+                </p>
+                
             </div>
         })
     return(
